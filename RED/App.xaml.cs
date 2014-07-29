5494246a4ac002295e77d96c52f28b3792f4ff2e
Ty Morrow <tjm6f4@mst.edu>
2014-07-29 18:05:27 -0500

    Simple startup status dialog.

--- RED/App.xaml.cs
+++ RED/App.xaml.cs
@@ -1,14 +1,27 @@
 ﻿namespace RED
 {
 	using System.Windows;
+	using Views;
 	using Views.ControlCenter;
 
 	public partial class App
 	{
 		private void Application_Startup(object sender, StartupEventArgs e)
 		{
-			var controlCenter = new ControlCenterView();
-			controlCenter.Show();
+			//Disable shutdown when the dialog closes
+			Current.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+
+			var dialog = new StartupStatusView();
+			dialog.Show();
+
+			var mainWindow = new ControlCenterView();
+
+			//Re-enable normal shutdown mode.
+			Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
+			Current.MainWindow = mainWindow;
+
+			dialog.Close();
+			mainWindow.Show();
 		}
 	}
 }
